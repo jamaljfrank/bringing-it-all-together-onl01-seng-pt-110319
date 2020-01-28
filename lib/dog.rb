69266854887bdb7624db369796bd3c4917db07f7
Jamal Frank <jamaljfrank@gmail.com>
2020-01-28 00:13:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,6 +55,15 @@ class Dog
     self.new(id: result[0], name: result[1], breed: result[2])
   end
   
-  
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !song.empty?
+      song_data = song[0]
+      song = Song.new(song_data[0], song_data[1], song_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
+  end
   
 end
