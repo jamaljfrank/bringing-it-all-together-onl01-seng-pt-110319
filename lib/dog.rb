c35a9f7c7149ffe83ffed064380d0684f1e73d84
Jamal Frank <jamaljfrank@gmail.com>
2020-01-28 00:27:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,5 +79,8 @@ class Dog
     end.first
   end
   
-  def 
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+      DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
 end
